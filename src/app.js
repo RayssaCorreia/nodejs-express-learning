1bb9c4ced86377983a8621667c0f89ae562a6f87
Rayssa Correia <rayssamicorreia@gmail.com>
2022-04-18 17:35:46 -0300

    Comando | Post

--- src/app.js
+++ src/app.js
@@ -8,8 +8,19 @@ const port = 4001
 server.use(json())
 server.use(cors())
 
+//enviar dados pelo servidor 
 server.get('/', (req, res) => {
     res.send("Hello World\n")
+})
+
+// server.post('/', function (req, res) {
+//     res.send('Olá !!!!');
+// });
+
+server.post('/', (req, res) => {
+    const request = req.body
+    res.send(request.usuarios)
+    res.status(201).send()
 })
 
 server.listen(port, (req, res) => {
