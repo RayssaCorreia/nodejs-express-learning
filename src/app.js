56c1b2a202467858efa5baccbe27e02c49db9978
Rayssa Correia <rayssamicorreia@gmail.com>
2022-04-25 17:37:15 -0300

    filtrando e apagando um id em especifico

--- src/app.js
+++ src/app.js
@@ -16,8 +16,15 @@ let contador = 0;
 //enviar dados pelo servidor 
 server.get('/', (req, res) => {
     //get responde as coisas que está em usuarios 
-    console.log(usuarios)
     res.status(200).send({usuarios})
+})
+
+//indentificar o que esta em um id 
+server.get('/:id', (req,res)=>{
+  const caminhoId = req.params.id
+  //o nome e igual ou seu id
+  const usuariosnome = usuarios.filter(nome => nome.id == caminhoId)
+  res.status(200).send(usuariosnome)
 })
 
 //solicitar a aceitação de dados (POST)
@@ -27,7 +34,7 @@ server.post('/', (req, res) => {
     const usuariosnome ={
         //que em um contador: maior igual a 1 
         id: contador += 1,
-        //Apos a contagem, ele pedi o que esta em nome
+        //ele pedi o que esta em nome
         nome: request.nome
     }
     // usuarios pucha o que esta no usuarios nome, junto com o id 
@@ -39,9 +46,33 @@ server.post('/', (req, res) => {
 // Apagar tudo que está dentro de usuarios
 server.delete('/', (req,res)=>{
     usuarios = [];
+    contador = 0;
                     //envia para o servidor
     res.status(200).send()
 })
+
+//Apagar um id/nome especifico 
+server.delet('/query',(req,res)=>{
+    const queryId = req.query.id
+    //vai filtrar e tirar o id igual a query
+    filtraLista = usuarios.filter(nome => nome.id != queryId)
+    //atualiza o usuarios como filtralista
+    usuarios = filtraLista
+    res.status(200).send()
+})
+
+// NÃO SEI O QUE ESTA ACONTECENDO 
+server.put('/:value',(req,res)=>{
+    const pathValue = req.params.value
+    const queryId = req.query.id
+    console.log(`QUERY É ${queryId} E PARAMETRO É ${pathValue}`)
+
+    usuarios.map(nome =>{
+    if(nome.id == queryId){
+        console.log(`ID ENCONTRADO ${queryId} ALTERANDO O VALOR DO OBJETO`)
+        nome.nome = pathValue
+    }
+});
 
 server.listen(port, (req, res) => {
     console.log(`server running on port ${port}`)
