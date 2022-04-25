b6666e3823c8dc825b90c05b503957e8a41abc0b
Rayssa Correia <rayssamicorreia@gmail.com>
2022-04-25 16:23:30 -0300

    requisição do nome + id

--- src/app.js
+++ src/app.js
@@ -11,23 +11,30 @@ server.use(cors())
 
 //Vetor: seu valor pode ser alterado
 let usuarios = [];
+let contador = 0;
 
 //enviar dados pelo servidor 
 server.get('/', (req, res) => {
     //get responde as coisas que está em usuarios 
-    res.send(`${usuarios}\n`)
-    res.status(200).send()
+    console.log(usuarios)
+    res.status(200).send({usuarios})
 })
 
 //solicitar a aceitação de dados (POST)
 server.post('/', (req, res) => {
-    //Trabalha com os dados mandados pelo nome ???
     const request = req.body
-    //pucha a requizição de usuarios. Que no caso é o nome 
-    usuarios.push(request.nome)
-    //se der certo mostrar esse código de https 
+    // uma variavel
+    const usuariosnome ={
+        //que em um contador: maior igual a 1 
+        id: contador += 1,
+        //Apos a contagem, ele pedi o que esta em nome
+        nome: request.nome
+    }
+    // usuarios pucha o que esta no usuarios nome, junto com o id 
+    usuarios.push(usuariosnome)
     res.status(201).send()
 })
+
 
 // Apagar tudo que está dentro de usuarios
 server.delete('/', (req,res)=>{
