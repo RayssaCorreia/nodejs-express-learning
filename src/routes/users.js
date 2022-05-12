2d35e7f13d481863acbefad76982f60180e9efff
Rayssa <97049575+RayssaCorreia@users.noreply.github.com>
2022-05-11 22:45:39 -0300

    Comentando

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,30 +1,43 @@
+// Ambiente do node.js
 const express = require('express')
+// Gerenciador/manipulador de rotas do NodeJS
 const router = express.Router()
 
-//mongoose
+// Mongoose (modelo no banco de dados)
 const Users = require('../modelos/userSenha')
 
+// Validador de senha e usuario
 const check = [(req, res, next) => {
 
+    // Verifica se o usuario, pedido é igual ao do banco de dados 
     Users.findOne({username: req.header('username')}).then( result => {
+        // User recebe o username do banco de dados
         const user = result.username; 
+        // Pass recebe o password do banco de dados
         const pass = result.password;
         
+        // Se o password(pesquisado) for diferente que pass
         if (req.header('password') != pass) {
-            console.log("Incorrect password")
+            // Printa no terminal:
+            console.log("Senha incorreta")
             return res.status(403).send()
+        // Se não 
         }else {
-            console.log("Correct password");
+            // Printa no terminal:
+            console.log("Senha correta");
             next()
         }
         
     }).catch(err => {
+        // se o nome estiver errado 
         return res.status(403).send("Incorrect username");
     })
 }]
 
+// Aqui é o lugar onde de fato puxa/pesquisa no banco de dados 
 router.get('/', check, (req, res) => {
-
+               // verificação
+    
     Users.find().then(user => {
         res.status(200).send(user);
 
@@ -33,6 +46,7 @@ router.get('/', check, (req, res) => {
     }) 
 })
 
+// Cria um usuario novo - Seguindo o modelo do banco de dados 
 router.post('/', (req,res) => {
     const user = new Users( {
 
