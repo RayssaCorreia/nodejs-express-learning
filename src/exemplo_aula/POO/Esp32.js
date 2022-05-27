beb4d17b3308e55987f9a7f0c0b1e52047e76866
Rayssa Correia <rayssamicorreia@gmail.com>
2022-05-27 17:40:42 -0300

    console

--- src/exemplo_aula/POO/Esp32.js
+++ src/exemplo_aula/POO/Esp32.js
@@ -8,3 +8,8 @@ class Esp32 extends DispositivoEletronico {
 }
 
 const Esp32 = new Esp32(38, 2, 2, "Preto","Wi-fi");
+console.log(`Ele contem ${Esp32.getPino()} pinos`);
+console.log(`Ele contem ${Esp32.getPino()} leds`);
+console.log(`Ele contem ${Esp32.getPino()} botões, sendo 1 programavel e outro para reset da placa`);
+console.log(`Sua cor ${Esp32.getPino()}`);
+console.log(`Ele está conectado no momento como ${Esp32.getPino()}`);
